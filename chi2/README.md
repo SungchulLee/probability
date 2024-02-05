1494aee6e2c92ab14a652224b704a2a5e2b1ecaa
SungchulLee <SungchulLee@users.noreply.github.com>
2024-02-05 18:31:28 +0900

    Delete chi2 directory

--- chi2/README.md
+++
@@ -1,5 +0,0 @@
-# chi2
-
-$$
-\sum_{i=1}^dZ_i^2\sim\chi^2_d\quad\quad\quad\quad\mbox{where$\quad$ $Z_i\quad$ iid $\quad N(0,1^2)$}
-$$
--- chi2/global_name_space.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='mnist')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-ARGS = parser.parse_args()
-
-ARGS.df = 5
